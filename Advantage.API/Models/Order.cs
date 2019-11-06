410a832b0c2c0341ba0ea17246825aa85124e10f
Fabrizio Alejandro Gelsi <fabrizio.gelsi@github.com>
2019-11-06 09:16:34 -0300

    WebAPI setup

---
+++ Advantage.API/Models/Order.cs
@@ -0,0 +1,13 @@
+namespace Advantage.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Placed { get; set; }
+        //? nullable because when someone place the order
+        // this one has not a date for completed field yet.
+        public DateTime? Completed { get; set;}
+    }
+}
