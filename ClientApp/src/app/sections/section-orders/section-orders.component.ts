95e564807b8e3e1fdd762336fc14f7966516376b
Fabrizio Alejandro Gelsi <fabrizio.gelsi@github.com>
2019-11-07 14:42:40 -0300

    Start pagination component

--- ClientApp/src/app/sections/section-orders/section-orders.component.ts
+++ ClientApp/src/app/sections/section-orders/section-orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Order } from '../../shared/order';
+import { SalesDataService } from '../../services/sales-data.service';
 
 @Component({
   selector: 'app-section-orders',
@@ -9,27 +10,40 @@ import { Order } from '../../shared/order';
 })
 export class SectionOrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesData: SalesDataService) { }
 
-  orders: Order[] = [
-    {id: 1, customer:
-      {id: 1, name: 'Main St Bakery', email: 'mains@example.com', state: 'CO'},
-      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017,12,2)},
-    {id: 2, customer:
-      {id: 1, name: 'Main St Bakery', email: 'mains@example.com', state: 'CO'},
-      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017,12,2)},
-    {id: 3, customer:
-      {id: 1, name: 'Main St Bakery', email: 'mains@example.com', state: 'CO'},
-      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017,12,2)},
-    {id: 4, customer:
-      {id: 1, name: 'Main St Bakery', email: 'mains@example.com', state: 'CO'},
-      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017,12,2)},
-    {id: 5, customer:
-      {id: 1, name: 'Main St Bakery', email: 'mains@example.com', state: 'CO'},
-      total: 230, placed: new Date(2017, 0, 1), fulfilled: new Date(2017,12,2)},
-  ];
+  orders: Order[];
+  total = 0;
+  page = 1;
+  limit = 10;
+  loading = false;
 
   ngOnInit() {
+    this.getOrders();
   }
 
+  getOrders(): void {
+    this._salesData.getOrders(this.page, this.limit)
+      .subscribe(res => {
+        console.log('Result from getOrders: ', res);
+        this.orders = res['page']['data'];
+        this.total = res['page'].total;
+        this.loading = false;
+      });
+  }
+
+  goToPrevious(): void {
+    this.page--;
+    this.getOrders();
+  }
+
+  goToNext(): void {
+    this.page++;
+    this.getOrders();
+  }
+
+  goToPage(n: number): void {
+    this.page = n;
+    this.getOrders();
+  }
 }
